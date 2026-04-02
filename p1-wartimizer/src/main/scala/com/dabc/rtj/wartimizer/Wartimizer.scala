2913182efad41480db6db69966a335c569e975e7
Darren Bishop <mail@darrenbishop.com>
2026-04-02 05:47:44 +0100

    Scala Macros & Metaprogramming: Wartimizer - Infrastructure and Scaffolding
    Signed-off-by: Darren Bishop <mail@darrenbishop.com>

--- p1-wartimizer/src/main/scala/com/dabc/rtj/wartimizer/Wartimizer.scala
+++ p1-wartimizer/src/main/scala/com/dabc/rtj/wartimizer/Wartimizer.scala
@@ -5,10 +5,21 @@ import quoted.*
 object Wartimizer {
   inline def wartimize[A](inline w: Wartimization, inline ws: Wartimization*)(block: A): A =
     ${ wartimizeImpl('w, 'ws, 'block) }
-      
+
   private def wartimizeImpl[A : Type](w: Expr[Wartimization], ws: Expr[Seq[Wartimization]], block: Expr[A])(using q: Quotes): Expr[A] = {
     import q.reflect.*
-    
-    ???
+
+    // get wartimization instances
+    val wartimizations = w.valueOrAbort +: ws.valueOrAbort
+
+    // get tree-maps
+    val treeMaps = wartimizations.map(_.treeMap)
+
+    // apply all tree-maps on the tree
+    val wartimized = treeMaps.foldLeft(block.asTerm) { (currTree: Tree, treeMap: TreeMap) =>
+      treeMap.transformTree(currTree)(Symbol.spliceOwner)
+    }
+
+    wartimized.asExprOf[A]
   }
 }
